96cb8a803e2b580d7f1a4b488fd351c97d7fdb81
Soumil32 <soumil009@outlook.com>
2022-08-19 22:22:02 +0100

    Limited the number of guesses
    On easy mode you have 3 guesses, 6 on medium and 8 on hard.

--- Guess the number/Program.cs
+++ Guess the number/Program.cs
@@ -6,36 +6,50 @@ public class Program
         Console.WriteLine("Choose a mode: easy, medium, or hard");
         string mode = Console.ReadLine();
         int maxNumber = 0;
-        
+        int maxGuesses = 0;
+        bool exitLoop = false;
+
         if (mode == "easy")
         {
             maxNumber = 11;
-        }else if (mode == "medium")
+            maxGuesses = 3;
+        }
+        else if (mode == "medium")
         {
             maxNumber = 31;
+            maxGuesses = 6;
         }
         else if (mode == "hard")
         {
             maxNumber = 51;
+            maxGuesses = 8;
         }
 
         while (true)
         {
+            exitLoop = false;
+            int timesGuessed = 0;
             Random random = new Random();
             int numberToGuess = random.Next(0, maxNumber);
 
-            Console.WriteLine("Guess a number between 0 and " + maxNumber);
-            int guess = int.Parse(Console.ReadLine());
-
-            while (guess != numberToGuess)
+            Console.WriteLine("Guess a number between 0 and " + (maxNumber - 1));
+            
+            while(!exitLoop)
             {
-                guess = int.Parse(Console.ReadLine());
-                if (guess == numberToGuess)
+                int guess = int.Parse(Console.ReadLine());
+                timesGuessed++;
+
+                if (timesGuessed == maxGuesses)
+                {
+                    Console.WriteLine("You lost. The number was " + numberToGuess);
+                    exitLoop = true;
+                }
+                else if (guess == numberToGuess)
                 {
                     Console.WriteLine("You guessed the number!");
+                    exitLoop = true;
                 }
             }
-            
         }
     }
 }
