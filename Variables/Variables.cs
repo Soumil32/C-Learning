48e24560d59082dbb41e01c60ad767426c6b1c68
Soumil32 <soumil009@outlook.com>
2022-08-19 20:14:08 +0100

    Wrote the program showcasing variables

--- Variables/Variables.cs
+++ Variables/Variables.cs
@@ -1,1 +1,17 @@
-﻿
+﻿namespace Variables
+{
+    class Variables
+    {
+        static public void Main(String[] args)
+        {
+            // Inisialized 2 integer variables named 'x' and 'y'
+            int x = 7;
+            // Set 'y' to be 3 more then 'x'
+            int y = x + 3;
+
+            // Printed 'y' to the console
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
