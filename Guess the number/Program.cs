bfacbb8876f38866bafcb48c211d14ad11ab6c5e
Soumil32 <soumil009@outlook.com>
2022-08-19 21:48:36 +0100

    Added modes
    The game now has easy, medium and hard modes!

--- Guess the number/Program.cs
+++ Guess the number/Program.cs
@@ -3,20 +3,39 @@ public class Program
 {
     public static void Main()
     {
-        while(true)
+        Console.WriteLine("Choose a mode: easy, medium, or hard");
+        string mode = Console.ReadLine();
+        int maxNumber = 0;
+        
+        if (mode == "easy")
+        {
+            maxNumber = 11;
+        }else if (mode == "medium")
+        {
+            maxNumber = 31;
+        }
+        else if (mode == "hard")
+        {
+            maxNumber = 51;
+        }
+
+        while (true)
         {
             Random random = new Random();
-            int numberToGuess = random.Next(0, 11);
+            int numberToGuess = random.Next(0, maxNumber);
 
+            Console.WriteLine("Guess a number between 0 and " + maxNumber);
             int guess = int.Parse(Console.ReadLine());
-            if (guess == numberToGuess)
+
+            while (guess != numberToGuess)
             {
-                Console.WriteLine("You guessed the number!");
+                guess = int.Parse(Console.ReadLine());
+                if (guess == numberToGuess)
+                {
+                    Console.WriteLine("You guessed the number!");
+                }
             }
-            else
-            {
-                Console.WriteLine("You didn't guess the number. It was " + numberToGuess);
-            }
+            
         }
     }
 }
