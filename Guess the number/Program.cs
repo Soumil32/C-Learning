805e70a93efa3290478661c3d6b8aa9fea4663aa
Soumil32 <soumil009@outlook.com>
2022-08-19 21:25:16 +0100

    Created a basic guess the number game

---
+++ Guess the number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        while(true)
+        {
+            Random random = new Random();
+            int numberToGuess = random.Next(0, 11);
+
+            int guess = int.Parse(Console.ReadLine());
+            if (guess == numberToGuess)
+            {
+                Console.WriteLine("You guessed the number!");
+            }
+            else
+            {
+                Console.WriteLine("You didn't guess the number. It was " + numberToGuess);
+            }
+        }
+    }
+}
