2f684f7d3d75e0df9e42a2a8b8d2541268ddc0d1
Soumil32 <soumil009@outlook.com>
2022-08-21 21:40:33 +0100

    Made modes not case sensitive

--- Guess the number/GuessTheNumber.cs
+++ Guess the number/GuessTheNumber.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 public class GuessTheNumber
 {
-public static bool ContainsCaseInsensitive(this string source, string substring)
-    {
-        return source?.IndexOf(substring, StringComparison.OrdinalIgnoreCase) > -1;
-    }
+    
 
     public static void Main()
     {
@@ -15,17 +12,17 @@ public static bool ContainsCaseInsensitive(this string source, string substring)
         int guess = -1;
         bool exitLoop = false;
 
-        if (mode.ContainsCaseInsensitive("easy")))
+        if (mode.ContainsCaseInsensitive("easy"))
         {
             maxNumber = 11;
             maxGuesses = 3;
         }
-        else if (mode.ContainsCaseInsensitive("medium")))
+        else if (mode.ContainsCaseInsensitive("medium"))
         {
             maxNumber = 31;
             maxGuesses = 6;
         }
-        else if (mode.ContainsCaseInsensitive("hard")))
+        else if (mode.ContainsCaseInsensitive("hard"))
         {
             maxNumber = 51;
             maxGuesses = 8;
@@ -39,26 +36,35 @@ public static bool ContainsCaseInsensitive(this string source, string substring)
             int numberToGuess = random.Next(0, maxNumber);
 
             Console.WriteLine("Guess a number between 0 and " + (maxNumber - 1));
-            
-            while(!exitLoop)
+
+            while (!exitLoop)
             {
-                
+
 
                 if (timesGuessed == maxGuesses)
                 {
                     Console.WriteLine("You lost. The number was " + numberToGuess);
                     exitLoop = true;
-                }else
-{
-    guess = int.Parse(Console.ReadLine());
-} 
+                }
+                else
+                {
+                    guess = int.Parse(Console.ReadLine());
+                }
                 if (guess == numberToGuess)
                 {
                     Console.WriteLine("You guessed the number!");
                     exitLoop = true;
-                } 
+                }
                 timesGuessed++;
             }
         }
+    }
+}
+
+public static class StringExtensions
+{
+    public static bool ContainsCaseInsensitive(this string source, string substring)
+    {
+        return source?.IndexOf(substring, StringComparison.OrdinalIgnoreCase) > -1;
     }
 }
