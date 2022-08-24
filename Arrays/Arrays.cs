5fbd21ac2f6be0aaee1c9345ebefbdf26c48ddf4
Soumil32 <soumil009@outlook.com>
2022-08-24 21:20:35 +0100

    Added comments

--- Arrays/Arrays.cs
+++ Arrays/Arrays.cs
@@ -6,12 +6,15 @@ namespace Arrays
 	{
 		static void Main(string[] args)
 		{
-			int[] numbers = new int[5];
+            /* Here is one way to create an array.
+			int[] numbers = new int[5]; The 5 stands for the maximum amount of things in the array
+			You can access a specific value from an array using its index
 			numbers[0] = 8;
 			numbers[1] = 64;
 			numbers[2] = 21;
 			numbers[3] = 12;
 			numbers[4] = 2;
-		}
-	}
+			*/
+        }
+    }
 }
