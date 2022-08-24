da5a74cbdc8f4d3974415897874d222ea987c612
Soumil32 <soumil009@outlook.com>
2022-08-24 20:54:17 +0100

    Added comments

--- For loops/Program.cs
+++ For loops/Program.cs
@@ -2,18 +2,18 @@
 
 namespace ForLoops
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            for(int i = 0; i < 10; i++)
-            {
-                if(i == 7)
-                {
-                    Console.WriteLine("I found seven");
-                    break;
-                }
-            }
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 0 /*Initialize a variable*/; i < 10 /* Add a condition*/; i++ /* Change the value*/)
+			{
+				if (i == 7) // Check if i is equals to 7
+				{
+					Console.WriteLine("I found seven");
+					break;
+				} //If it isn't add 1 to i and run it again if it is still under 10
+			}
+		}
+	}
 }
