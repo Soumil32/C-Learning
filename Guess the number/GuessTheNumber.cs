84ae73a66ccc42b85186c1f683595e5ebff27a7e
Soumil32 <soumil009@outlook.com>
2022-08-24 23:05:18 +0100

    Replace the "ContainsCaseInsensitive" method with the one in more type extensions

--- Guess the number/GuessTheNumber.cs
+++ Guess the number/GuessTheNumber.cs
@@ -1,68 +1,60 @@
 ﻿using System;
+using MoreTypeExtensions;
+
 public class GuessTheNumber
 {
-    
+	public static void Main()
+	{
+		Console.WriteLine("Choose a mode: easy, medium, or hard");
+		string mode = Console.ReadLine();
+		int maxNumber = 0;
+		int maxGuesses = 0;
+		int guess = -1;
 
-    public static void Main()
-    {
-        Console.WriteLine("Choose a mode: easy, medium, or hard");
-        string mode = Console.ReadLine();
-        int maxNumber = 0;
-        int maxGuesses = 0;
-        int guess = -1;
+		if (mode.ContainsCaseInsensitive("easy"))
+		{
+			maxNumber = 11;
+			maxGuesses = 3;
+		}
+		else if (mode.ContainsCaseInsensitive("medium"))
+		{
+			maxNumber = 31;
+			maxGuesses = 6;
+		}
+		else if (mode.ContainsCaseInsensitive("hard"))
+		{
+			maxNumber = 51;
+			maxGuesses = 8;
+		}
 
-        if (mode.ContainsCaseInsensitive("easy"))
-        {
-            maxNumber = 11;
-            maxGuesses = 3;
-        }
-        else if (mode.ContainsCaseInsensitive("medium"))
-        {
-            maxNumber = 31;
-            maxGuesses = 6;
-        }
-        else if (mode.ContainsCaseInsensitive("hard"))
-        {
-            maxNumber = 51;
-            maxGuesses = 8;
-        }
+		while (true)
+		{
+			int timesGuessed = 0;
+			Random random = new Random();
+			int numberToGuess = random.Next(0, maxNumber);
 
-        while (true)
-        {
-            int timesGuessed = 0;
-            Random random = new Random();
-            int numberToGuess = random.Next(0, maxNumber);
+			Console.WriteLine("Guess a number between 0 and " + (maxNumber - 1));
 
-            Console.WriteLine("Guess a number between 0 and " + (maxNumber - 1));
-
-            while(true)
-            {
+			while(true)
+			{
 
 
-                if (timesGuessed == maxGuesses)
-                {
-                    Console.WriteLine("You lost. The number was " + numberToGuess);
-                    break;
-                }
-                else
-                {
-                    guess = int.Parse(Console.ReadLine());
-                }
-                if (guess == numberToGuess)
-                {
-                    Console.WriteLine("You guessed the number!");
-                    break;
-                }
-                timesGuessed++;
-            }
-        }
-    }
-}
-
-public static class StringExtensions
-{
-    public static bool ContainsCaseInsensitive(this string source, string substring)
-    {
-        return source?.IndexOf(substring, StringComparison.OrdinalIgnoreCase) > -1;
-    }
+				if (timesGuessed == maxGuesses)
+				{
+					Console.WriteLine("You lost. The number was " + numberToGuess);
+					break;
+				}
+				else
+				{
+					guess = int.Parse(Console.ReadLine());
+				}
+				if (guess == numberToGuess)
+				{
+					Console.WriteLine("You guessed the number!");
+					break;
+				}
+				timesGuessed++;
+			}
+		}
+	}
 }
