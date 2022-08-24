1c64de24bebfd10aa1cf26e13353194f3fb61ba0
Soumil32 <soumil009@outlook.com>
2022-08-24 21:15:54 +0100

    Created an array of numbers

--- Arrays/Arrays.cs
+++ Arrays/Arrays.cs
@@ -6,7 +6,12 @@ namespace Arrays
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] numbers = new int[5];
+			numbers[0] = 8;
+			numbers[1] = 64;
+			numbers[2] = 21;
+			numbers[3] = 12;
+			numbers[4] = 2;
 		}
 	}
 }
