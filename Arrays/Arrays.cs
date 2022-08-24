b1167dcdec646af41f4aac36c72aa647917002d7
Soumil32 <soumil009@outlook.com>
2022-08-24 21:27:49 +0100

    Added another way to declare a variable.

--- Arrays/Arrays.cs
+++ Arrays/Arrays.cs
@@ -6,15 +6,19 @@ namespace Arrays
 	{
 		static void Main(string[] args)
 		{
-            /* Here is one way to create an array.
-			int[] numbers = new int[5]; The 5 stands for the maximum amount of things in the array
-			You can access a specific value from an array using its index
+			// Here is one way to create an array.
+			int[] numbers = new int[5]; // The 5 stands for the maximum amount of things in the array.
+			// You can access a specific value from an array using its index.
 			numbers[0] = 8;
 			numbers[1] = 64;
 			numbers[2] = 21;
 			numbers[3] = 12;
 			numbers[4] = 2;
-			*/
+
+            // Here we don't specify how many numbers you want.
+			// Instead, the compiler just uses how many numbers you typed.
+			// It also looks alot cleaner.
+            int[] numbers2 = new int[] {8, 64, 21, 12, 2};
         }
     }
 }
