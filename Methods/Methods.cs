65bd2739606cc3149ce5bdf9454da0c19622fd9d
Soumil32 <soumil009@outlook.com>
2022-08-25 22:20:48 +0100

    Added more code

--- Methods/Methods.cs
+++ Methods/Methods.cs
@@ -2,9 +2,6 @@
 {
 	class Methods
 	{
-		static void Main(string[] args)
-		{
-		}
 
 		/* 
 		This is a simple method that writes 'Hello World' to the console.
@@ -12,10 +9,18 @@
 		The word 'private' means that you can only use it in this class.
 		(Create a note for what static means later)
 		The word void means it just executes the code inside it and doesn't return anything.
+		Next you give the method a name which is followed by a pair of paranthensis.
+		Lastly, you write your code between a pair of curly brackets.
 		*/
 		private static void HelloWorld()
 		{
 			Console.WriteLine("Hello World");
 		}
-	}
+
+        static void Main(string[] args)
+        {
+			// To call a function you just type its name followed by a pair of parathensis
+			HelloWorld();
+        }
+    }
 }
