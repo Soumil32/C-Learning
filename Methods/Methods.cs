9cb7e596ebb04d526c02577ea05084a45bda8667
Soumil32 <soumil009@outlook.com>
2022-08-25 22:15:18 +0100

    Created and commented the hello world function

--- Methods/Methods.cs
+++ Methods/Methods.cs
@@ -1,10 +1,21 @@
 ﻿namespace Methods
 {
-    class Methods
-    {
-        static void Main(string[] args)
-        {
+	class Methods
+	{
+		static void Main(string[] args)
+		{
+		}
 
-        }
-    }
+		/* 
+		This is a simple method that writes 'Hello World' to the console.
+		With methods you can write code once and use it again easily.
+		The word 'private' means that you can only use it in this class.
+		(Create a note for what static means later)
+		The word void means it just executes the code inside it and doesn't return anything.
+		*/
+		private static void HelloWorld()
+		{
+			Console.WriteLine("Hello World");
+		}
+	}
 }
