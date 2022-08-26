1a52844d4a4afa96df90082a8e7f9d898428632a
Soumil32 <soumil009@outlook.com>
2022-08-26 16:19:27 +0100

    Created the strings project

---
+++ Strings/Strings.cs
@@ -0,0 +1,12 @@
+﻿namespace Strings
+{
+	class Strings
+	{
+		static void Main(string[] args)
+		{
+			string myString = "My \"so-called\" life";
+			Console.WriteLine(myString);
+			Console.ReadLine();
+		}
+	}
+}
