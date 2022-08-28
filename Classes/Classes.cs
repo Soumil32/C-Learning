0acae520dfd411ff01cc04daa431d97d92c2d91b
Soumil32 <soumil009@outlook.com>
2022-08-28 08:56:27 +0100

    Created the classes project

---
+++ Classes/Classes.cs
@@ -0,0 +1,22 @@
+﻿namespace Classes
+{
+	class Class
+	{
+		static void Main(string[] args)
+		{
+			Car myCar = new Car();
+			myCar.brand = "Nissan";
+
+			Console.WriteLine(myCar);
+		}
+	}
+
+	class Car
+	{
+		public string brand { get; set; }
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public string Year { get; set; }
+		public string Colour { get; set; }
+	}
+}
