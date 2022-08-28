ade08ae3026a68db4c2318ae9068b08f2a6c2e50
Soumil32 <soumil009@outlook.com>
2022-08-28 08:57:53 +0100

    Created a program which solves equations8

---
+++ Equation/Equation.cs
@@ -0,0 +1,21 @@
+﻿namespace Equations
+{
+    class Equations
+    {
+        static void Main(string[] args)
+        {
+            //x + 3x = 56
+            // x + 3x = x + 45
+            int x = int.MinValue;
+
+            while (x + (x * 3)!= x + 45)
+            {
+                x ++;
+                
+            }
+
+            Console.WriteLine(x);
+            File.WriteAllText(@"C:\Users\soumi\Documents\GitHub\C-Learning\Equation\Answer.txt", x.ToString());
+        }
+    }
+}
