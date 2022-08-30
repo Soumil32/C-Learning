04c5eafde2f4b0d6c91395d038aab4b8633755ca
Soumil32 <soumil009@outlook.com>
2022-08-30 18:18:56 +0100

    Created the ObjectLifeTime project

---
+++ ObjectLifeTime/ObjectLifeTime.cs
@@ -0,0 +1,19 @@
+﻿namespace ObjectLifeTime
+{
+    class MainClass
+    {
+        // ReSharper disable once ArrangeTypeMemberModifiers
+        static void Main(string[] args)
+        {
+            Car myCar = new();
+        }
+    }
+
+    class Car
+    {
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public int? Year { get; set; }
+        public string? Colour { get; set; }
+    }
+}
