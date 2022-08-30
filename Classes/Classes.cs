11d4f3213ed7916f8d0c34c69d38148b644006bb
Soumil32 <soumil009@outlook.com>
2022-08-30 18:09:36 +0100

    Update Classes.cs

--- Classes/Classes.cs
+++ Classes/Classes.cs
@@ -2,21 +2,37 @@
 {
 	class Class
 	{
+		// ReSharper disable once UnusedParameter.Local
 		static void Main(string[] args)
 		{
 			Car myCar = new Car();
-			myCar.brand = "Nissan";
+			myCar.Make = "Toyota";
+			myCar.Model = "Prius";
+			myCar.Year = 2018;
+			myCar.Colour = "Red";
 
-			Console.WriteLine(myCar);
+			Console.WriteLine("{0} {1} {2} {3}", myCar.Make,myCar.Model, myCar.Year, myCar.Colour);
+			Console.WriteLine("{0:C}", myCar.DetermineCarValue());
 		}
 	}
 
 	class Car
 	{
-		public string brand { get; set; }
-		public string Make { get; set; }
-		public string Model { get; set; }
-		public string Year { get; set; }
-		public string Colour { get; set; }
+		public string? Make { get; set; }
+		public string? Model { get; set; }
+		public int? Year { get; set; }
+		public string? Colour { get; set; }
+
+		public decimal DetermineCarValue()
+		{
+			decimal carValue = 0;
+
+			if (Year > 2005)
+				carValue += 800;
+			else
+				carValue += 200;
+
+			return carValue;
+		}
 	}
 }
