131b7229b197f6a0a831be73957e7df1a976eedc
Soumil32 <soumil009@outlook.com>
2022-09-03 16:22:38 +0100

    Created a project for benchmarking code

---
+++ BenchmarkCode/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using MoreTypeExtensions;
+
+namespace BenchmarkCode
+{
+    [MemoryDiagnoser()]
+    [RankColumn]
+    public class CodeToBenchmark
+    {
+        static void Main()
+        {
+            var summary = BenchmarkRunner.Run<CodeToBenchmark>();
+        }
+
+        [Benchmark]
+        public string ReverseStringWithNewString()
+        {
+            string stringToReverse = "I am going to wake up tonight not tomorrow";
+            char[] stringToReverseCharArray = stringToReverse.ToCharArray();
+            Array.Reverse(stringToReverseCharArray);
+            return new string(stringToReverseCharArray);
+        }
+        
+        public string ReverseStringWithStringConcat()
+        {
+            string stringToReverse = "I am going to wake up tonight not tomorrow";
+            char[] stringToReverseCharArray = stringToReverse.ToCharArray();
+            Array.Reverse(stringToReverseCharArray);
+            return String.Concat(stringToReverseCharArray);
+        }
+        
+        [Benchmark]
+        public string ReverseStringWithStringFormat()
+        {
+            string stringToReverse = "I am going to wake up tonight not tomorrow";
+            char[] stringToReverseCharArray = stringToReverse.ToCharArray();
+            Array.Reverse(stringToReverseCharArray);
+            return string.Format($"{stringToReverseCharArray}");
+        }
+    }
+}
